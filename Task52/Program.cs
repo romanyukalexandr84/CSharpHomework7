b37cf140eb7d96cc87632ed319fefa3475e2bd9d
romanukalexandr <romanukalexandr@gmail.com>
2023-03-04 08:42:05 +0300

    add Task52

---
+++ Task52/Program.cs
@@ -0,0 +1,54 @@
+﻿void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Average (int[,] matr)
+{
+    double[] total = new double [matr.GetLength(1)];
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        for (int i = 0; i < matr.GetLength(0); i++)
+        {
+            total [j] += matr[i,j];
+        }
+        Console.Write((total[j] / matr.GetLength(0)) + "||");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите количество строк и столбцов матрицы");
+int rows = Convert.ToInt32(Console.ReadLine());
+int columns = Convert.ToInt32(Console.ReadLine());
+
+while ((rows <= 0) || (columns <= 0))
+{
+    Console.WriteLine("Введите числа больше нуля");
+    rows = Convert.ToInt32(Console.ReadLine());
+    columns = Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] matrix = new int[rows, columns];
+FillArray(matrix);
+PrintArray(matrix);
+
+Console.WriteLine("Среднее арифметическое каждого столбца:");
+Average(matrix);
