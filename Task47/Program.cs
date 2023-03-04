c7962e9ac9c3801d45a7adf6e209da77530d4c0f
romanukalexandr <romanukalexandr@gmail.com>
2023-03-04 06:29:26 +0300

    add Task47

---
+++ Task47/Program.cs
@@ -0,0 +1,39 @@
+﻿void FillArray(double[,] matr)
+{
+    Random random = new Random();
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = Math.Round(random.NextDouble()*100, 2);
+        }
+    }
+}
+
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + "  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк и столбцов матрицы");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
+while ((m <= 0) || (n <= 0))
+{
+    Console.WriteLine("Введите числа больше нуля");
+    m = Convert.ToInt32(Console.ReadLine());
+    n = Convert.ToInt32(Console.ReadLine());
+}
+
+double[,] matrix = new double[m, n];
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
